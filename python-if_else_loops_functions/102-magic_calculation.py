473fbc1ce5b9b07bec2bd302bdbc7970599b446d
Marie Joselyne Nyampinga <j.nyampinga@alustudent.com>
2023-02-07 08:00:52 -0800

    102-magic_calculation.py

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
-    if c < b:
+    if c > b:
         return (a + b)
     return (a*b - c)

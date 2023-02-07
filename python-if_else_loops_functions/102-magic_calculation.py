c9f99412f8462a703d9e229a37f65aa6716a2f57
Marie Joselyne Nyampinga <j.nyampinga@alustudent.com>
2023-02-07 07:54:43 -0800

    102-magic_calculation.py

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-#102-magic_calculation.py
+# 102-magic_calculation.py
 
 def magic_calculation(a, b, c):
     if a < b:
-        return
+        return(c)
     if c < b:
-        return(a +b)
-    return (a*b-c)
+        return(a + b)
+    return (a*b - c)

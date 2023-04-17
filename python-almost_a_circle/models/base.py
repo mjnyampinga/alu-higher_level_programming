529b33863c895d1af5ce76c36740d0239c2f9149
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-04-17 17:54:55 +0000

    test

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""A base class"""
+"""
+Base class
+"""
+
 
 import json
-import turtle
-import csv
 
 
 class Base:
-    """A base class"""
+    """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new id"""
         if id is not None:
             self.id = id
         else:
@@ -20,117 +20,53 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Convert a list of dictionaries to a JSON string"""
+        """Dictionary to JSON string"""
         if list_dictionaries is None:
             return "[]"
-
-        if len(list_dictionaries) == 0:
-            return "[]"
-
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Save a list of objects to a file"""
-        file_name = cls.__name__ + ".json"
-        new_list = []
-        if list_objs:
-            for i in list_objs:
-                new_list.append(cls.to_dictionary(i))
+        """
+        the JSON string to a file
+        """
+        filename = cls.__name__ + ".json"
 
-        with open(file_name, mode="w") as myFile:
-            myFile.write(cls.to_json_string(new_list))
+        with open(filename, "w", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                new_list = [ins.to_dictionary() for ins in list_objs]
+                f.write(cls.to_json_string(new_list))
 
     @staticmethod
     def from_json_string(json_string):
-        """Convert a JSON string to a list of dictionaries"""
-        if json_string is None:
+        """
+        the JSON string to dictonary
+        """
+        if json_string is None or len(json_string) == 0:
             return []
-
-        if len(json_string) == 0:
-            return []
-
-        list_dicts = json.loads(json_string)
-        return list_dicts
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Create a new object"""
+        """Dictionary to instance"""
         if cls.__name__ == "Rectangle":
-            dummy = cls(3, 2)
+            dummy_obj = cls(4, 5)  # dummy instance
         if cls.__name__ == "Square":
-            dummy = cls(3)
-        dummy.update(**dictionary)
-        return dummy
+            dummy_obj = cls(4)  # dummy instance
+        dummy_obj.update(**dictionary)
+        return dummy_obj
 
     @classmethod
     def load_from_file(cls):
-        """Load a list of objects from a file"""
+        """File to instance"""
+        filename = cls.__name__ + ".json"
         try:
-            with open(cls.__name__ + ".json", "r") as file:
-                content = file.read()
-        except FileNotFoundError:
+            with open(filename, 'r', encoding='utf-8') as f:
+                a_list = cls.from_json_string(f.read())
+                return [cls.create(**item) for item in a_list]
+        except:
             return []
-
-        ex_content = cls.from_json_string(content)
-        context_list = []
-        for instance_dict in ex_content:
-            context_list.append(cls.create(**instance_dict))
-        return context_list
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """ Save to a CSV file """
-        fn = cls.__name__ + ".csv"
-        if fn == "Rectangle.csv":
-            fields = ["id", "width", "height", "x", "y"]
-        else:
-            fields = ["id", "size", "x", "y"]
-        with open(fn, mode="w", newline="") as myFile:
-            if list_objs is None:
-                writer = csv.writer(myFile)
-                writer.writerow([[]])
-            else:
-                writer = csv.DictWriter(myFile, fieldnames=fields)
-                writer.writeheader()
-                for x in list_objs:
-                    writer.writerow(x.to_dictionary())
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """ Load from a CSV file """
-        try:
-            fn = cls.__name__ + ".csv"
-            with open(fn, newline="") as myFile:
-                reader = csv.DictReader(myFile)
-                lst = []
-                for x in reader:
-                    for i, n in x.items():
-                        x[i] = int(n)
-                    lst.append(x)
-                return ([cls.create(**objt) for objt in lst])
-        except FileNotFoundError:
-            return ([])
-
-    @staticmethod
-    def draw(list_rectangles, list_squares):
-        """ Draw the rectangles and squares """
-        shapes = []
-        if list_rectangles:
-            shapes.extend(list_rectangles)
-        if list_squares:
-            shapes.extend(list_squares)
-        pen = turtle.Turtle()
-        pen.pen(pencolor='black', pendown=False, pensize=2, shown=False)
-        for shape in shapes:
-            pen.penup()
-            pen.setpos(shape.x, shape.y)
-            pen.pendown()
-            pen.forward(shape.width)
-            pen.right(90)
-            pen.forward(shape.height)
-            pen.right(90)
-            pen.forward(shape.width)
-            pen.right(90)
-            pen.forward(shape.height)
-            pen.right(90)
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,88 +1,63 @@
 #!/usr/bin/python3
-"""Create a Square class for the work"""
+"""
+the class square that inherits from Rectangle
+"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Create a Square class for the work"""
+    """define class Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Create a Square class for the work
-
-        :param size: The size of the square
-        :param x: The x coordinate of the square
-        :param y: The y coordinate of the square
-        :param id: The id of the square
-        """
+        """initialization"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
-        """
-        Return the size of the square
-
-        :return: The size of the square
-        """
+        """Getter size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        Set the size of the square
-
-        :param value: The size of the square
-        """
+        """Setter size"""
         self.width = value
         self.height = value
 
     def __str__(self):
-        """
-        Return the string representation of the square
-
-        :return: The string representation of the square
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        """prints [Square] (<id>) <x>/<y> - <size>"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+                self.__class__.__name__, self.id, self.x, self.y,
+                self.size)
 
     def update(self, *args, **kwargs):
-        """
-        Update the square
-
-        :return: The updated square
-        """
-        if len(args) != 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[2]
-                self.y = args[3]
-            except IndexError:
-                pass
-
-        elif len(kwargs) != 0:
+        """square update"""
+        if args:
+            for cnt, arg in enumerate(args):
+                if cnt == 0:
+                    self.id = arg
+                elif cnt == 1:
+                    self.size = arg
+                elif cnt == 2:
+                    self.x = arg
+                else:
+                    self.y = arg
+        else:
             if "id" in kwargs:
                 self.id = kwargs["id"]
-            else:
-                self.id
-
             if "size" in kwargs:
                 self.size = kwargs["size"]
-            else:
-                self.size
-
             if "x" in kwargs:
                 self.x = kwargs["x"]
-            else:
-                self.x
-
             if "y" in kwargs:
                 self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """
-        Return a dictionary representation of the square
-
-        :return: The dictionary representation of the square
-        """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        """Square instance to dictionary representation"""
+        dic = {}
+        dic["id"] = self.id
+        dic["size"] = self.size
+        dic["x"] = self.x
+        dic["y"] = self.y
+        return dic

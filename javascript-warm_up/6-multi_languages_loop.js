883d0eb44a0e6aa3d9f60d242ae21661d7e683eb
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-06 09:39:11 +0000

    task6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i = 0; i < languages.length; i++) {
-  console.log(languages[i]);
+const rows = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < rows.length; i++) {
+  console.log(rows[i]);
 }

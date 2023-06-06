c78145fe9ca77238ad5de5d99a333b37cc9e93da
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-06 11:13:06 +0000

    task8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,12 +2,12 @@
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size) || size <= 0) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     let row = "";
     for (let j = 0; j < size; j++) {
-      row += "X";
+      row += 'X';
     }
     console.log(row);
   }

c16034106ebfa173190ab32138d267ffef2431f8
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-06 15:09:30 +0000

    task9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-const a = parseInt(process.argv[2], 10);
-const b = parseInt(process.argv[3], 10);
-function add (a, b) {
-  return (a + b);
-}
-console.log(add(a, b));
+const add = (a, b) => {
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+  } else {
+    console.log(a + b);
+  }
+};
+
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));

1ca9856471b487b5aed29c9df1cfcf897126d636
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-06 09:18:08 +0000

    task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argsCount = process.argv.length - 2;
+const args = process.argv.slice(2);
 
-if (argsCount === 0) {
-  console.log("No argument");
-} else if (argsCount === 1) {
-  console.log("Argument found");
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }

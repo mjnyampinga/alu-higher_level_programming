530cacaf87677222ef8a5fdf7b4933c6f3de44d5
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-06 15:01:32 +0000

    task9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a + b;
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+function add (a, b) {
+  return (a + b);
 }
-
-if (process.argv.length !== 4) {
-  console.log("Usage: node 9-add.js <integer1> <integer2>");
-} else {
-  const a = parseInt(process.argv[2]);
-  const b = parseInt(process.argv[3]);
-
-  if (isNaN(a) || isNaN(b)) {
-    console.log("NaN");
-  } else {
-    const result = add(a, b);
-    console.log(result);
-  }
-}
+console.log(add(a, b));

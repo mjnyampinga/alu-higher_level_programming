7668dba78f21c609d1327f3b8e566c8a466e0d41
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-07 14:55:13 +0000

    task3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        return {};
-      }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    }
     this.width = w;
     this.height = h;
-    }
-  
-    print() {
-      for (let i = 0; i < this.height; i++) {
-      let row = "";
+  }
+  print() {
+  for (let i = 0; i < this.height; i++) {
+  let row = "";
       for (let j = 0; j < this.width; j++) {
           row += "X";
         }
@@ -19,4 +18,4 @@ class Rectangle {
   }
 }
 module.exports = Rectangle;
-  
+ 

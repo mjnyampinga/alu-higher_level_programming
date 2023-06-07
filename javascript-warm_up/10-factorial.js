c24e26f15b10bcf5d61a314a90f46737d2f4aa52
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-07 13:53:39 +0000

    task10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-function factorial(n) {
+
+function factorial (n) {
   if (isNaN(n)) {
     return 1;
   } else if (n === 0) {
@@ -7,12 +8,6 @@ function factorial(n) {
   } else {
     return n * factorial(n - 1);
   }
-};
-
-if (process.argv.length !== 3) {
-  console.log(result);
-} else {
-  const num = parseInt(process.argv[2]);
-  const result = factorial(num);
-  console.log(result);
 }
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));

1fb265f178560a485dc037e80fdf14e41ecc84f0
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-07 15:10:47 +0000

    task3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,13 +8,13 @@ class Rectangle {
     this.height = h;
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
         row += 'X';
       }
-       console.log(row);
+      console.log(row);
     }
   }
 }

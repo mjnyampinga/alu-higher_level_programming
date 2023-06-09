1f7ef3eedda3e12426e3e2714fe35483a362fa22
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-09 14:32:09 +0000

    task1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,10 @@
-#!/usr/bin/node
-const req = require('request');
-req.get(process.argv[2], (err, res) => {
-  err ? console.log(err) : console.log('code: ' + res.statusCode);
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
 });

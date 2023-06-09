1995432237b976481f0fd051aa947b37438a34da
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-09 15:01:58 +0000

    task7

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
+
 const request = require('request');
+const path = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-const movieID = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-
-request(apiUrl, (error, response, body) => {
+request(path, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
+    const result = JSON.parse(body);
+    for (const item of result.characters) {
+      request(item, function (error, response, body) {
         if (error) {
           console.error(error);
         } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
+          console.log(JSON.parse(body).name);
         }
       });
-    });
+    }
   }
 });

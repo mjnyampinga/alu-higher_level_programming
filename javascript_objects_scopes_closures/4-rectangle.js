c5e7c141fd1095d96d92f68aca412210e818c4bf
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-06-09 10:05:13 +0000

    task4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    print () {
-      for (let i = 0; i < this.height; i++) {
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
-      }
     }
+  }
     rotate () {
       const tempWidth = this.width;
     this.width = this.height;
